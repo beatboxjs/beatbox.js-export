dd7f408c528a5c98b757212a08817f067ef290e6
Candid Dauth <cdauth@cdauth.eu>
2015-05-06 16:26:54 +0200

    Working version that can export MP3 and WAV

--- src/export.js
+++ src/export.js
@@ -1,31 +1,4 @@
-Beatbox.prototype.export = function() {
-	function getWave(instr, callback) {
-		if(instr.bbWave)
-			return callback(bbWave);
-
-		async.waterfall([
-			function(next) {
-				if(instr.soundObj.bbWave)
-					return next(null, instr.soundObj.bbWave);
-
-				JSZipUtils.getBinaryContent(qualifyURL(instr.soundObj._urls[0]), function(err, data) {
-					console.log(err, data);
-					if(err)
-						return console.error(err);
-
-					AV.Asset.fromBuffer(data).decodeToBuffer(function(buffer) {
-						instr.soundObj.bbWave = buffer;
-						next(null, buffer);
-					});
-				});
-			},
-			function(soundWave, next) {
-				instr.bbWave = soundWave; // TODO: Sprite
-
-				callback(null, instr.bbWave);
-			}
-		]);
-	}
+(function() {
 
 	function arrayKeys(arr) {
 		var ret = [ ];
@@ -40,24 +13,118 @@ Beatbox.prototype.export = function() {
 		return el.firstChild.href;
 	}
 
-	var bufferL = [ ];
-	var bufferR = [ ];
+	var e = Beatbox.Export = {
+		_getWaveForInstrument : function(instrumentObj, callback) {
+			if(typeof instrumentObj.bbWave != "undefined")
+				return callback(instrumentObj.bbWave);
 
-	var i = 0;
-	var p = this._pattern;
-	async.eachSeries(arrayKeys(p), function(i, next) {
-		async.eachSeries(arrayKeys(p[i]), function(j, next) {
-			var instrWithParams = Beatbox._getInstrumentWithParams(p[i][j]);
-			if(!instrWithParams)
-				return next();
+			async.waterfall([
+				function(next) {
+					if(typeof instrumentObj.soundObj.bbWave != "undefined")
+						return next(null, instrumentObj.soundObj.bbWave);
 
-			getWave(instrWithParams.instrumentObj, function(wave) {
-				console.log(wave);
+					e._decodeToBuffer(AV.Asset.fromURL(qualifyURL(instrumentObj.soundObj._urls[0])), function(err, buffer) {
+						if(err)
+							console.error("Error decoding "+instrumentObj.soundObj._urls[0]+":", err);
 
-				next();
+						instrumentObj.soundObj.bbWave = buffer || null;
+
+						next(null, buffer);
+					});
+				},
+				function(soundWave, next) {
+					// TODO: Respect Sprite, channel count, rate
+					instrumentObj.bbWave = soundWave;
+
+					callback(instrumentObj.bbWave);
+				}
+			]);
+		},
+
+		_decodeToBuffer : function(asset, callback) {
+			asset.decodeToBuffer(function(buffer) {
+				callback(null, buffer);
 			});
-		}, next);
-	}, function(err) {
-		console.log("done");
-	});
-};
+			asset.on("error", function(err) {
+				callback(err);
+			});
+		},
+
+		_encodeWAV : function(left, right, callback) {
+			WavEncoder.encode({
+				sampleRate: 44100,
+				channelData: [
+					left,
+					right
+				]
+			}).then(function(buffer) {
+				callback(new Blob([ buffer ], { type: "audio/wav" }));
+			})
+		},
+
+		_encodeMP3 : function(left, right, callback) {
+			var mp3codec = Lame.init();
+			Lame.set_mode(mp3codec, Lame.JOINT_STEREO);
+			Lame.set_num_channels(mp3codec, 2);
+			Lame.set_in_samplerate(mp3codec, 44100);
+			Lame.set_out_samplerate(mp3codec, 44100);
+			Lame.set_bitrate(mp3codec, 128);
+			Lame.init_params(mp3codec);
+
+			var data = [ ];
+			var bufferLength = 20000;
+			for(var i=0; i<left.length; i+=bufferLength) {
+				data.push(Lame.encode_buffer_ieee_float(mp3codec, left.subarray(i, i+bufferLength), right.subarray(i, i+bufferLength)).data);
+			}
+
+			data.push(Lame.encode_flush(mp3codec).data);
+
+			Lame.close(mp3codec);
+
+			callback(new Blob(data, { type: "audio/mp3" }));
+		},
+
+		_getBufferForPattern : function(pattern, strokeLength, callback) {
+			var bufferL = new Array(Math.ceil(pattern.length*strokeLength*44.1));
+			var bufferR = new Array(Math.ceil(pattern.length*strokeLength*44.1));
+			async.eachSeries(arrayKeys(pattern), function(i, next) {
+				async.eachSeries(arrayKeys(pattern[i]), function(j, next) {
+					var instrWithParams = Beatbox._getInstrumentWithParams(pattern[i][j]);
+					if(!instrWithParams)
+						return next();
+
+					e._getWaveForInstrument(instrWithParams.instrumentObj, function(wave) {
+						if(wave) {
+							for(var pos=Math.floor(i*strokeLength*44.1),waveIdx=0; waveIdx<wave.length; pos++,waveIdx+=1) {
+								if(!bufferL[pos])
+									bufferL[pos] = 0;
+								bufferL[pos] += wave[waveIdx];
+
+								if(!bufferR[pos])
+									bufferR[pos] = 0;
+								bufferR[pos] += wave[waveIdx];
+							}
+						}
+
+						next();
+					});
+				}, next);
+			}, function() {
+				callback(new Float32Array(bufferL), new Float32Array(bufferR));
+			});
+		}
+	};
+
+	Beatbox.prototype.exportMP3 = function(callback) {
+		e._getBufferForPattern(this._pattern, this._strokeLength, function(left, right) {
+			e._encodeMP3(left, right, callback);
+		});
+	};
+
+	Beatbox.prototype.exportWAV = function(callback) {
+		e._getBufferForPattern(this._pattern, this._strokeLength, function(left, right) {
+			e._encodeWAV(left, right, callback);
+		});
+	};
+
+})();
